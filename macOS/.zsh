2097e8a73e1b9010d1eac3a32e4563fcaf904bce
Jan Oehen <jan.oe.hen@live.com>
2022-03-14 16:42:00 +0100

    Adding variables to profiles

--- macOS/.zsh
+++ macOS/.zsh
@@ -2,3 +2,5 @@ cat << EOF >> ~/.zprofile
 # Add Visual Studio Code (code)
 export PATH="\$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
 EOF
+
+export VAGRANT_DEFAULT_PROVIDER=parallels
