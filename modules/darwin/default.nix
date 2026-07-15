0b440014ebad027d8baacfe5fa4c2bccc07b95e4
Jan Oehen <10827844+Oechiih@users.noreply.github.com>
2026-07-15 20:09:29 +0200

    mitchellh inspired changes

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -10,20 +10,11 @@
   networking.computerName = "MacGuffin";
   networking.hostName = "MacGuffin";
 
+  system.primaryUser = "joe";
+
   environment.shells = with pkgs; [ nushell ];
 
-  # Set XDG_CONFIG_HOME for shell sessions (zsh, bash)
   environment.variables.XDG_CONFIG_HOME = "$HOME/.config";
-
-  # Set it in the launchd user session so nushell sees it when used as login shell
-  launchd.user.agents."setenv-xdg-config-home" = {
-    serviceConfig = {
-      ProgramArguments = [
-        "/bin/launchctl" "setenv" "XDG_CONFIG_HOME" "/Users/joe/.config"
-      ];
-      RunAtLoad = true;
-    };
-  };
 
   users.users.joe.home = "/Users/joe";
 
